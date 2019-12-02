a0ed1c3e5c2e774cedec5969fda570f6be94c80e
Fabio Andrade <fabio.andrade@basis.com.br>
2019-12-02 18:51:52 -0300

    Criação do detalhamento do restaurant

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -13,8 +13,16 @@ export class RestaurantsService {
 
     constructor(private http: Http){}
 
+    //json-server db.json
+
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError);
+    }
+
+    restaurantById(id: string): Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError);
     }
